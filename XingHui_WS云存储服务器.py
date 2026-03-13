2e99e4229fc09949a1e1382048b20cb0688044f6
VovaLU <vova1525@foxmail.com>
2026-03-14 00:13:44 +0800

    Add files via upload

--- XingHui_WS云存储服务器.py
+++ XingHui_WS云存储服务器.py
@@ -1,4 +1,5 @@
 # 该程序由15岁自学者初学半个多月开发,之后加上云变量同步逻辑,希望不要见笑了,很多写的东西简直是垃圾awa
+# 该项目的开源链接:https://github.com/vovahub/XingHui-Cloud-Storage-WebSocket-Server
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 from threading import Thread
 import json
@@ -9,7 +10,7 @@ import gc
 import psutil
 import random
 
-服务器版本 = "0.1.3"
+服务器版本 = "0.1.4"
 服务器版本更新内容 = [
     "0.0.1_作者大脑褶皱被抚平🧠✋突然想做个主要面向图形化编程的好用的云存储,并打算用那微薄的python知识随便写一个MVP"
     "~0.0.4_刚完成正常功能,完成基础WS服务器+存储逻辑",
@@ -21,7 +22,8 @@ import random
     "0.1.0_修改了用户查看账户状态时'您还可用(MB)'的浮点精度问题",
     "0.1.1_修复了逻辑漏洞(全新的UAT账户初次调用时,因对应的数据目录尚未创建,在计算存储占用时触发了“系统找不到指定路径”的WinError错误)",
     "0.1.2_做完了第一个BETA版本的RtCVS功能,但还有bug或许需要修一修",
-    "0.1.3_修复了'路径遍历攻击'漏洞;我的天十分抱歉我的电脑基础知识不够,已经修复了(QMQ没人说过还有回退路径这个玩法啊...eee)"
+    "0.1.3_修复了'路径遍历攻击'漏洞;我的天十分抱歉我的电脑基础知识不够,已经修复了(QMQ没人说过还有回退路径这个玩法啊...EEE)",
+    "0.1.4_修改了UAT格式让其支持自定义权限系统并在代码中支持Ciallo～(∠・ω< )⌒☆"
 ]
 欢迎语 = '''~~~欢迎语👏and免责约言~~~
 欢迎接入星辉服务器！储纳之用，分文不取。诸君数据，必守秘如瓶。然稳妥计，还望自备密文加密之法为善。
@@ -41,7 +43,7 @@ import random
 !----附言​----!
 服务器运维实需成本，为爱发电固佳，若蒙捐助以破会员之制，则幸甚至哉！
 支付宝/声讯：13275773306
-（穷连冰糕亦难求，乐助五元足感盛情！泣谢QwQ）
+（穷连冰糕亦难求，乐助五元足盛情！泣谢QwQ）
 '''
 print(r'''
  __  __   __   __   __   ______   __  __   __  __   __   __     __   ______    
@@ -84,7 +86,7 @@ try:
                         "read": True, # 读取数据
                         "write": True, # 写入数据
                         "delete": True, # 删除数据
-                        "cv_write": True, # 使用云变量上传数据服务
+                        "cv": True, # 使用云变量数据服务
                     }
                 }
             ]
@@ -149,9 +151,14 @@ def 获取目录大小(路径):
                 总字节 += 获取目录大小(entry.path)
         return 总字节
     except Exception as e:
-        # 在该环境下提示报错不是一个好方法,这会让我看到错误感到焦虑
+        # # 在该环境下提示报错不是一个好方法,这会让我看到错误感到焦虑
         # rizhi.warning(f"[目录占用计算函数]发生了错误但为了保证运行正常忽略|ERROR:'{e}'")
         return 0
+
+class Permission_error(Exception):
+    pass
+class Operation_exception(Exception):
+    pass
 
 Client = {
     "all":[],
@@ -242,16 +249,20 @@ class MyServer(WebSocket):
                     # ---------声明客户端数据访问操作类型---------
                     if 接收数据_key == "type":
                         if 接收数据_value == "RtCVS":
+                            if not 该用户的UAT["permissions"]["cv"]:
+                                raise Permission_error(f"客户端({self.address}|{该用户的UAT["token"]})的操作在UAT信息中没有操作权限('cv')")
                             if 该用户的UAT["token"] not in Client["type"]["RtCVS"]:
                                 Client["type"]["RtCVS"][该用户的UAT["token"]] = []
                             Client["type"]["RtCVS"][该用户的UAT["token"]].append(self)
                             rizhi.info(f"[TYPE]客户端'{self.address}'声明自己为'RtCVS'类型")
                     # ---------云存储部分_数据基础操作---------
                     if 接收数据_key in ["dump","write","w"]:  # 存储文件
+                        if not 该用户的UAT["permissions"]["write"]:
+                            raise Permission_error(f"客户端({self.address}|{该用户的UAT["token"]})的操作在UAT信息中没有操作权限('write')")
                         dump_文件名 = 接收数据_value[0]
-                        for 字符 in dump_文件名:
+                        for 字符 in dump_文件名: # 简单有效的防范路径漏洞
                             if 字符 in 文件名黑名单:
-                                raise ValueError(f"客户端可能试图进行路径遍历(路径回退上一级漏洞)攻击(字符'{字符}'包含在'{文件名黑名单}'中)")
+                                raise ValueError(f"客户端或许试图进行路径遍历(路径回退上一级漏洞)攻击(字符'{字符}'属于违规字符)")
                         dump_数据 = 接收数据_value[1]
                         if len(str(dump_数据).encode('utf-8')) < 该用户的UAT["storage_limit"]*1024*1024 - 该用户的UAT["used_storage"]*1024*1024:
                             os.makedirs(f"{数据目录}/{该用户的UAT["token"]}", exist_ok=True)
@@ -275,9 +286,11 @@ class MyServer(WebSocket):
                             self.sendMessage(json.dumps({"ERROR":理由}, indent=2, ensure_ascii=False))
                             rizhi.warning(f"[DUMP_ERROR]{理由}")
                     if 接收数据_key in ["load","read","r"]:  # 读取文件
-                        for 字符 in 接收数据_value:
+                        if not 该用户的UAT["permissions"]["read"]:
+                            raise Permission_error(f"客户端({self.address}|{该用户的UAT["token"]})的操作在UAT信息中没有操作权限('read')")
+                        for 字符 in 接收数据_value: # 简单有效的防范路径漏洞
                             if 字符 in 文件名黑名单:
-                                raise ValueError(f"客户端可能试图进行路径遍历(路径回退上一级漏洞)攻击(字符'{字符}'包含在'{文件名黑名单}'中)")
+                                raise ValueError(f"客户端或许试图进行路径遍历(路径回退上一级漏洞)攻击(字符'{字符}'属于违规字符)")
                         with open(f"{数据目录}/{该用户的UAT["token"]}/{接收数据_value}.txt", "r", encoding="utf-8") as f:
                             读取数据 = f.read()
                         self.sendMessage(读取数据)
@@ -287,10 +300,14 @@ class MyServer(WebSocket):
                             显示数据 = 读取数据
                         rizhi.info(f"[LOAD]客户端({self.address}|{该用户的UAT["token"]})进行了查阅数据|数据名'{接收数据_value}';内容'{显示数据}'")
                     if 接收数据_key in ["remove","delete","del"]: # 删除文件
+                        if not 该用户的UAT["permissions"]["delete"]:
+                            raise Permission_error(f"客户端({self.address}|{该用户的UAT["token"]})的操作在UAT信息中没有操作权限('delete')")
                         os.remove(f"{数据目录}/{该用户的UAT["token"]}/{接收数据_value}.txt")
                         rizhi.info(f"[REMOVE]客户端({self.address}|{该用户的UAT["token"]})进行了删除数据|数据名'{接收数据_value}'")
                     # ---------实时同步云变量部分(RtCVS/CV)_数据操作---------
                     if 接收数据_key in ["CV_dump","CV_write","CV_w"]:
+                        if not 该用户的UAT["permissions"]["cv"]:
+                            raise Permission_error(f"客户端({self.address}|{该用户的UAT["token"]})的操作在UAT信息中没有操作权限('cv')")
                         需要减少可用次数 = False
                         if 该用户的UAT["token"] not in RtCVS:
                             RtCVS[该用户的UAT["token"]] = {}
@@ -340,8 +357,8 @@ class MyServer(WebSocket):
                         self.sendMessage(欢迎语)
                         rizhi.info(f"[i_tos]客户端({self.address}|{该用户的UAT["token"]})进行了查看tos信息")
             except Exception as e:
-                rizhi.warning(f"客户端({self.address})可能发送了无用或错误的信息导致处理失败|原始数据{self.data}|错误:{e}")
-
+                self.sendMessage(rizhi.warning(f"客户端({self.address})可能发送了无用或错误的信息导致处理失败|原始数据{self.data}|错误:{e}"))
+                return # 直接返回终止在这里,不进行收尾工作,删掉这一行代表不管出现什么错误都会进行收尾工作,这可能产生不必要的报错日志
             #-----收尾工作,为该用户UAT信息做调整-----
             if 需要减少可用次数:
                 该用户的UAT["total_uses"] -= 1
